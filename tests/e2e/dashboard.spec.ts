486ae6cee430e8a2215e8efd39910951833f7bbf
David Bomba <turbo124@gmail.com>
2023-11-01 16:31:40 +1100

    Dashboard tests

---
+++ tests/e2e/dashboard.spec.ts
@@ -0,0 +1,21 @@
+import { login, logout, permissions } from '$tests/e2e/helpers';
+import test, { expect } from '@playwright/test';
+
+test("see dashboard after login", async ({ page }) => {
+    const { clear, save } = permissions(page);
+
+    await login(page);
+    await clear();
+    await save();
+    await logout(page);
+
+    await login(page, 'permissions@example.com', 'password');
+
+    await expect(page.locator('.flex-grow > .flex-1').first()).toContainText(
+        'Dashboard'
+    );
+});
+
+test("can see search", async ({ page }) => {
+
+});
