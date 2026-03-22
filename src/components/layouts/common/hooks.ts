e0276d5ff3d64b13e9914a10f7ae8a1f744e47ba
David Bomba <turbo124@gmail.com>
2026-03-22 12:29:39 +1100

    minor fixes

--- src/components/layouts/common/hooks.ts
+++ src/components/layouts/common/hooks.ts
@@ -191,14 +191,6 @@ export function useSettingsRoutes() {
       enabled: ((isAdmin || isOwner) && isCompanySettingsActive) || false,
     },
     {
-      name: t('workflows'),
-      href: '/workflows',
-      current:
-        location.pathname.startsWith('/workflows') ||
-        location.pathname.startsWith('/workflow_runs'),
-      enabled: ((isAdmin || isOwner) && isCompanySettingsActive) || false,
-    },
-    {
       name: t('system_logs'),
       href: '/settings/system_logs',
       current: location.pathname.startsWith('/settings/system_logs'),
